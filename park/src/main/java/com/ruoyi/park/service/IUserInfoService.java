f8e2898b1273e8eb053dc55a4b21a8dd17187d4d
bigbigcar <1677830702@qq.com>
2024-04-29 14:22:58 +0800

    bigcar: 修改了部分数据库的数据，实现了用户、管理员用户的增删改查

---
+++ park/src/main/java/com/ruoyi/park/service/IUserInfoService.java
@@ -0,0 +1,61 @@
+package com.ruoyi.park.service;
+
+import com.ruoyi.park.domain.UserInfo;
+
+import java.util.List;
+
+/**
+ * 查询用户Service接口
+ *
+ * @author bigcar
+ * @date 2024-04-29
+ */
+public interface IUserInfoService {
+    /**
+     * 查询查询用户
+     *
+     * @param id 查询用户主键
+     * @return 查询用户
+     */
+    public UserInfo selectUserInfoById(Long id);
+
+    /**
+     * 查询查询用户列表
+     *
+     * @param userInfo 查询用户
+     * @return 查询用户集合
+     */
+    public List<UserInfo> selectUserInfoList(UserInfo userInfo);
+
+    /**
+     * 新增查询用户
+     *
+     * @param userInfo 查询用户
+     * @return 结果
+     */
+    public int insertUserInfo(UserInfo userInfo);
+
+    /**
+     * 修改查询用户
+     *
+     * @param userInfo 查询用户
+     * @return 结果
+     */
+    public int updateUserInfo(UserInfo userInfo);
+
+    /**
+     * 批量删除查询用户
+     *
+     * @param ids 需要删除的查询用户主键集合
+     * @return 结果
+     */
+    public int deleteUserInfoByIds(String ids);
+
+    /**
+     * 删除查询用户信息
+     *
+     * @param id 查询用户主键
+     * @return 结果
+     */
+    public int deleteUserInfoById(Long id);
+}
---
+++ park/src/main/java/com/ruoyi/park/service/impl/UserInfoServiceImpl.java
@@ -0,0 +1,88 @@
+package com.ruoyi.park.service.impl;
+
+import com.ruoyi.common.core.text.Convert;
+import com.ruoyi.park.domain.UserInfo;
+import com.ruoyi.park.mapper.UserInfoMapper;
+import com.ruoyi.park.service.IUserInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 查询用户Service业务层处理
+ *
+ * @author bigcar
+ * @date 2024-04-29
+ */
+@Service
+public class UserInfoServiceImpl implements IUserInfoService {
+    @Autowired
+    private UserInfoMapper userInfoMapper;
+
+    /**
+     * 查询查询用户
+     *
+     * @param id 查询用户主键
+     * @return 查询用户
+     */
+    @Override
+    public UserInfo selectUserInfoById(Long id) {
+        return userInfoMapper.selectUserInfoById(id);
+    }
+
+    /**
+     * 查询查询用户列表
+     *
+     * @param userInfo 查询用户
+     * @return 查询用户
+     */
+    @Override
+    public List<UserInfo> selectUserInfoList(UserInfo userInfo) {
+        return userInfoMapper.selectUserInfoList(userInfo);
+    }
+
+    /**
+     * 新增查询用户
+     *
+     * @param userInfo 查询用户
+     * @return 结果
+     */
+    @Override
+    public int insertUserInfo(UserInfo userInfo) {
+        return userInfoMapper.insertUserInfo(userInfo);
+    }
+
+    /**
+     * 修改查询用户
+     *
+     * @param userInfo 查询用户
+     * @return 结果
+     */
+    @Override
+    public int updateUserInfo(UserInfo userInfo) {
+        return userInfoMapper.updateUserInfo(userInfo);
+    }
+
+    /**
+     * 批量删除查询用户
+     *
+     * @param ids 需要删除的查询用户主键
+     * @return 结果
+     */
+    @Override
+    public int deleteUserInfoByIds(String ids) {
+        return userInfoMapper.deleteUserInfoByIds(Convert.toStrArray(ids));
+    }
+
+    /**
+     * 删除查询用户信息
+     *
+     * @param id 查询用户主键
+     * @return 结果
+     */
+    @Override
+    public int deleteUserInfoById(Long id) {
+        return userInfoMapper.deleteUserInfoById(id);
+    }
+}
